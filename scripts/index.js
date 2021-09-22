9b92ea8572c02dc8298ae5f7e2d2d4f1f6cb166e
Chase Nicks <chasenicks81@gmail.com>
2021-09-22 15:43:38 -0400

    small changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $(".navbar-burger").click(function() {
+$(document).ready(function () {
+    $(".navbar-burger").click(function () {
         $(".navbar-burger, .navbar-menu", $(this).closest('.navbar')).toggleClass("is-active");
     });
-  });
+});
