6caaeac75ad2cc393b5cba0794559e9a3d07747d
Chase Nicks <chasenicks81@gmail.com>
2021-09-25 14:53:03 -0400

    animated content scrolling

---
+++ scripts/scroll.js
@@ -0,0 +1,18 @@
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+});
+
+sr.reveal(`#hero-tag, #hero-sub-tag`,{delay: 200});
+sr.reveal(`#aboutme`,{origin: 'top', delay: 200});
+sr.reveal(`#aboutme-para`,{origin: 'bottom', delay: 200});
+sr.reveal(`.skill-name`,{origin: 'bottom', delay: 400});
+sr.reveal(`.icon-skill`,{origin: 'top', delay: 400});
+sr.reveal(`.skill-name-p`,{origin: 'left', delay: 500});
+sr.reveal(`.list-title`,{origin: 'right', delay: 500});
+sr.reveal(`.skill-details`,{origin: 'bottom', delay: 500});
+sr.reveal(`.work-t`,{origin: 'top', delay: 300});
+sr.reveal(`.work-p`,{origin: 'bottom', delay: 300});
+sr.reveal(`.is-3by2`,{origin: 'bottom', delay: 300});
